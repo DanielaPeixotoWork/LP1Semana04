c1b62582dff53b1a306eb87956ef790563af2259
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-03-13 21:14:42 +0000

    String

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -6,7 +6,23 @@ namespace BetterDecorator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: dotnet run --project BetterDecorator -- <string> <char> <int>");
+                return;
+            }
+
+            string inputString = args[0];
+            string decorationChar = args[1];
+            int decorationCount = 0;
+
+            if (!int.TryParse(args[2], out decorationCount))
+            {
+                Console.WriteLine("Third argument must be an integer.");
+                return;
+            }
+
         }
+
     }
 }
